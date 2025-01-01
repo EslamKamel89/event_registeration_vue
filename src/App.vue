5c7c5491913278337831e82be0c1e68c98da673c
EslamKamel89 <elsamkamelforex@gmail.com>
2025-01-01 11:35:23 +0200

    tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script lang="ts">
-export default {}
+<script setup lang="ts">
+console.log('hello world');
 </script>
 
 <template>
-  <h2>hello world</h2>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl font-medium">Event Booking App</h1>
+    <h2 class="text-2xl font-medium">All Events</h2>
+    <h2 class="text-2xl font-medium">Your Booking</h2>
+  </main>
 </template>
-
-<style></style>
