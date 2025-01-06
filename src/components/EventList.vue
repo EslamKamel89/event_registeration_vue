01f920dd80e9262640219b0a0cca517d85952b52
EslamKamel89 <elsamkamelforex@gmail.com>
2025-01-06 11:53:42 +0200

    handling event empty state

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -2,14 +2,19 @@
   <h2 class="text-2xl font-medium">All Events</h2>
   <section class="grid grid-cols-1 md:grid-cols-2 gap-8">
     <template v-if="!eventLoading">
-      <EventCard
-        v-for="ev in events"
-        :key="ev.id"
-        :title="ev.title"
-        :when="ev.date"
-        :description="ev.description"
-        @register="(id: string) => emit('register', ev)"
-      />
+      <template v-if="events.length > 0">
+        <EventCard
+          v-for="ev in events"
+          :key="ev.id"
+          :title="ev.title"
+          :when="ev.date"
+          :description="ev.description"
+          @register="(id: string) => emit('register', ev)"
+        />
+      </template>
+      <template v-else>
+        <div class="col-span-2 text-gray-700 text-center">No Events Right Now</div>
+      </template>
     </template>
     <template v-else>
       <LoadingEventCard v-for="i in 8" :key="i" />
