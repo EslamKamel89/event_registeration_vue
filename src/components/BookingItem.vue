340678b518640005b9fef0b03bf593d521cf1fbd
EslamKamel89 <elsamkamelforex@gmail.com>
2025-01-08 11:33:47 +0200

    adding dynamic component for the state

--- src/components/BookingItem.vue
+++ src/components/BookingItem.vue
@@ -4,6 +4,9 @@
       <div class="flex space-x-2 items-center">
         <div>{{ title }}</div>
         <div class="border border-gray-200 rounded-full p-3">{{ status }}</div>
+        <div>
+          <component :is="icon" :class="{ 'animate-spin': pending }" />
+        </div>
       </div>
       <VueButton variant="danger" @click="cancelRegisteration(bookingId)"
         ><template #content>Cancel</template></VueButton
@@ -13,8 +16,10 @@
 </template>
 <script setup lang="ts">
 import SectionCard from '@/components/SectionCard.vue';
+import { Check, LoaderCircle } from 'lucide-vue-next';
+import { computed } from 'vue';
 import VueButton from './VueButton.vue';
-defineProps<{
+const props = defineProps<{
   title: string;
   bookingId: string;
   status: 'pending' | 'canceled' | 'confirmed';
@@ -27,4 +32,11 @@ const emit = defineEmits<{
 const cancelRegisteration = async (bookingId: string) => {
   emit('canceled', bookingId);
 };
+
+const pending = computed<boolean>(() => {
+  return props.status === 'pending';
+});
+const icon = computed<typeof LoaderCircle | typeof Check>(() => {
+  return pending.value ? LoaderCircle : Check;
+});
 </script>
